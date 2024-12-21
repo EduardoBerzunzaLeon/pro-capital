efecafc92d7720331f2d803efc90ca4f86e5cbd8
Eduardo Jesus Berzunza Leon <eduardoberzunzal@gmail.com>
2024-12-20 21:53:28 -0600

    Render fase 3

--- app/.server/adapter/db.ts
+++ app/.server/adapter/db.ts
@@ -10,13 +10,12 @@ declare global {
 
 if (process.env.NODE_ENV === "production") {
     db = new PrismaClient()
-    db.$connect()
 } else {
     if (!global.__db) {
         global.__db = new PrismaClient();
-        global.__db.$connect();
     }
     db = global.__db;
+    global.__db.$connect();
 }
 
 export { db };
